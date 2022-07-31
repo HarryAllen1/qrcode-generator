20d4c604cde720aa756ef62b40559b025508e926
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-07-30 19:02:07 -0700

    Potentially fix errors

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	esbuild: {
+		banner: {
+			js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
+		},
+	},
 };
 
 export default config;
