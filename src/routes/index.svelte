c61ff41be8c2af836a4285df0a4ea25df7a174c1
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-08-09 00:39:42 -0700

    finish implementation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -37,13 +37,9 @@
 	$: transparentBackground = false;
 	$: foregroundColor = '#000000';
 	$: backgroundColor = '#ffffff';
-	$: storedBgColor = backgroundColor;
-
-	let transparentSliderDisabled = false;
 
 	$: if (selectedFormat.format === 'jpg') {
-		transparentBackground = false;
-		transparentSliderDisabled = true;
+		backgroundColor = '#000';
 	}
 
 	let message: string;
@@ -220,11 +216,10 @@
 				<SwitchGroup as="div" class="flex items-center space-x-4">
 					<SwitchLabel class="text-black dark:text-white">Transparent Background</SwitchLabel>
 					<Switch
-						disabled={transparentSliderDisabled}
 						as="button"
 						checked={transparentBackground}
 						on:change={(event) => {
-							backgroundColor = event.detail ? '#0000' : storedBgColor;
+							backgroundColor = event.detail ? '#0000' : '#fff';
 							transparentBackground = event.detail;
 						}}
 						class={({ checked }) =>
@@ -232,17 +227,34 @@
 								'relative inline-flex flex-shrink-0 h-6 border-2 border-transparent rounded-full cursor-pointer w-11 focus:outline-none focus:shadow-outline transition-colors ease-in-out duration-200',
 								checked ? 'bg-blue-600' : 'bg-gray-400'
 							)}
-						let:checked
 					>
 						<span
 							class={classNames(
 								'inline-block w-5 h-5 bg-white rounded-full transform transition ease-in-out duration-200',
-								checked ? 'translate-x-5' : 'translate-x-0'
+								transparentBackground ? 'translate-x-5' : 'translate-x-0'
 							)}
 						/>
 					</Switch>
 				</SwitchGroup>
 			</div>
+			{#if selectedFormat.format === 'jpg' && transparentBackground}
+				<p class="w-72 text-gray-500 dark:text-gray-400 flex flex-row">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						aria-hidden="true"
+						height="26"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					JPGs don't support transparent backgrounds, making the background black.
+				</p>
+			{/if}
 		</details>
 	</div>
 </div>
