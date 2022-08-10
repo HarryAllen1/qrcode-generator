75cdad50a5f731751063764e4cfb17ef8eb75998
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-08-09 21:53:40 -0700

    Add some OG tags and favicon

--- src/lib/dark-mode.ts
+++ src/lib/dark-mode.ts
@@ -1,3 +1,14 @@
 import { writable } from 'svelte/store';
 
 export const state = writable(false);
+
+state.subscribe((v) => {
+	if ('document' in globalThis)
+		if (v) {
+			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+			document.head.querySelector<HTMLLinkElement>('[rel=icon]')!.href = '/favicon-dark.png';
+		} else {
+			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+			document.head.querySelector<HTMLLinkElement>('[rel=icon]')!.href = '/favicon.png';
+		}
+});
