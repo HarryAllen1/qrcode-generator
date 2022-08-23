3cc1b9b96ee525c178ec417fe376dc2da2b63d33
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-08-23 15:14:43 -0700

    tests in different browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,40 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices, type PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	testDir: 'tests',
 	webServer: {
 		command: 'pnpm run build && pnpm run preview',
 		port: 4173,
 	},
+	fullyParallel: true,
+	projects: [
+		//--begin-chromium
+		{
+			name: 'chromium',
+			use: {
+				...devices['Desktop Chrome'],
+			},
+		},
+		//--end-chromium
+
+		//--begin-firefox
+		{
+			name: 'firefox',
+			use: {
+				...devices['Desktop Firefox'],
+			},
+		},
+		//--end-firefox
+
+		//--begin-webkit
+		{
+			name: 'webkit',
+			use: {
+				...devices['Desktop Safari'],
+			},
+		},
+		//--end-webkit
+	],
 };
 
 export default config;
