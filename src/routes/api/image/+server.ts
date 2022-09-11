b9ca927031d5be82b830766f7483f448807ec8bd
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-09-10 20:28:42 -0700

    hmmmm..... issues

--- src/routes/api/image/+server.ts
+++ src/routes/api/image/+server.ts
@@ -1,13 +1,13 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const POST: RequestHandler = async () => {
-	// const { qrCodeImageDataURL, width }: Record<string, string> =
-	// 	await request.json();
+export const POST: RequestHandler = async ({ request }) => {
+	const { qrCodeImageDataURL, width }: Record<string, string> =
+		await request.json();
 
-	// if (!width) return json({ error: 'Missing width' }, { status: 400 });
-	// if (!qrCodeImageDataURL)
-	// 	return json({ error: 'Missing qrCodeImageDataURL' }, { status: 400 });
+	if (!width) return json({ error: 'Missing width' }, { status: 400 });
+	if (!qrCodeImageDataURL)
+		return json({ error: 'Missing qrCodeImageDataURL' }, { status: 400 });
 
 	// const canvas = createCanvas(parseFloat(width), parseFloat(width));
 	// const ctx = canvas.getContext('2d');
