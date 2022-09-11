c2a76507a409cd3784b95a49393c32e1a154e620
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-09-10 19:59:04 -0700

    fix x2

--- src/routes/api/image/+server.ts
+++ src/routes/api/image/+server.ts
@@ -1,25 +1,25 @@
-import { createCanvas, loadImage } from '@napi-rs/canvas';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const POST: RequestHandler = async ({ request }) => {
-	const { qrCodeImageDataURL, width }: Record<string, string> =
-		await request.json();
+export const POST: RequestHandler = async () => {
+	// const { qrCodeImageDataURL, width }: Record<string, string> =
+	// 	await request.json();
 
-	if (!width) return json({ error: 'Missing width' }, { status: 400 });
-	if (!qrCodeImageDataURL)
-		return json({ error: 'Missing qrCodeImageDataURL' }, { status: 400 });
+	// if (!width) return json({ error: 'Missing width' }, { status: 400 });
+	// if (!qrCodeImageDataURL)
+	// 	return json({ error: 'Missing qrCodeImageDataURL' }, { status: 400 });
 
-	const canvas = createCanvas(parseFloat(width), parseFloat(width));
-	const ctx = canvas.getContext('2d');
-	ctx.drawImage(
-		await loadImage(qrCodeImageDataURL),
-		0,
-		0,
-		canvas.width,
-		canvas.height
-	);
-	return json({
-		imageDataURL: canvas.toDataURL(),
-	});
+	// const canvas = createCanvas(parseFloat(width), parseFloat(width));
+	// const ctx = canvas.getContext('2d');
+	// ctx.drawImage(
+	// 	await loadImage(qrCodeImageDataURL),
+	// 	0,
+	// 	0,
+	// 	canvas.width,
+	// 	canvas.height
+	// );
+	// return json({
+	// 	imageDataURL: canvas.toDataURL(),
+	// });
+	return json({});
 };
