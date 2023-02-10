5cd2fbb9d5a1e8fafc27304e0a8ec0313fe050d0
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2023-02-10 11:08:55 -0800

    fix building

--- svelte.config.js
+++ svelte.config.js
@@ -3,19 +3,18 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true,
-		sourceMap: true,
-	}),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess({
+    postcss: true,
+    sourceMap: true,
+  }),
 
-	kit: {
-		adapter: adapter({
-			// cannot use sharp in edge functions :(
-			edge: false,
-		}),
-	},
+  kit: {
+    adapter: adapter({
+      runtime: 'node18',
+    }),
+  },
 };
 
 export default config;
