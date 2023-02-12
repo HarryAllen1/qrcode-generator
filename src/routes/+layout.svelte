ba09f71f696c8f63edf10c10ce7c3e9a387db6de
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-02-11 21:06:45 -0800

    fix errors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import '$lib/pre.scss';
 	import '$lib/tailwind.scss';
 	import { onMount } from 'svelte';
+	import './global.scss';
 
 	onMount(async () => {
 		if (browser && !dev) {
@@ -13,38 +14,3 @@
 </script>
 
 <slot />
-
-<style lang="scss" global>
-	.dark .prose {
-		--tw-prose-headings: #fff;
-	}
-
-	.dark .prose code {
-		background-color: #282c34;
-		color: #abb2bf;
-		border-radius: 4px;
-		padding: 3px 6px;
-
-		&::before,
-		&::after {
-			content: '';
-		}
-	}
-
-	.prose code {
-		background-color: #eaeaeb;
-		color: #383a42;
-		border-radius: 4px;
-		padding: 3px 6px;
-		transition: background-color 0.5s, color 0.5s;
-
-		&::before,
-		&::after {
-			content: '';
-		}
-	}
-
-	body {
-		transition: background-color 0.5s, color 0.5s;
-	}
-</style>
